910c9bf7cde093d611382aceb08de4c3d1cec197
Devin Powell <dpowel11@kent.edu>
2019-07-30 20:25:43 -0600

    Updates

---
+++ precache-manifest.dc7ec465837f5425706087f5d5d4a625.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafdb05ff97d283a061bd60e94c8e665",
+    "url": "/clickingBad/index.html"
+  },
+  {
+    "revision": "8692af037a4182797337",
+    "url": "/clickingBad/static/css/main.22cd9597.chunk.css"
+  },
+  {
+    "revision": "10413514992144727419",
+    "url": "/clickingBad/static/js/2.9f039cbc.chunk.js"
+  },
+  {
+    "revision": "8692af037a4182797337",
+    "url": "/clickingBad/static/js/main.7c1968f3.chunk.js"
+  },
+  {
+    "revision": "d843fd40ffb1c5ee87af",
+    "url": "/clickingBad/static/js/runtime~main.b79bacff.js"
+  },
+  {
+    "revision": "baaff21a4c33db17c1067801bb30a214",
+    "url": "/clickingBad/static/media/buried.baaff21a.jpg"
+  },
+  {
+    "revision": "4065cdf61f2e81481bd001ee77d2e274",
+    "url": "/clickingBad/static/media/fullMeasure.4065cdf6.jpg"
+  },
+  {
+    "revision": "f13050c5d8f2d3438a4560d21dba452c",
+    "url": "/clickingBad/static/media/janeMargolis.f13050c5.jpg"
+  },
+  {
+    "revision": "5ac542c20be6dd6317d81df228d4b9be",
+    "url": "/clickingBad/static/media/jesseCar.5ac542c2.jpg"
+  },
+  {
+    "revision": "6414c7e9414d5469967e7449e87d2c40",
+    "url": "/clickingBad/static/media/madrigal.6414c7e9.jpg"
+  },
+  {
+    "revision": "cbdb7c39f307305a93d717aa86e6de8e",
+    "url": "/clickingBad/static/media/salamancaBros.cbdb7c39.jpg"
+  },
+  {
+    "revision": "373a17abb9919238fb1fe1f64083701f",
+    "url": "/clickingBad/static/media/saulGoodman.373a17ab.jpg"
+  },
+  {
+    "revision": "e57917ad124a5c9c06c6be1de713cbea",
+    "url": "/clickingBad/static/media/skinnyPete.e57917ad.jpg"
+  },
+  {
+    "revision": "bfc078fadca0e84e2c505cb75eb11f17",
+    "url": "/clickingBad/static/media/waltJesse.bfc078fa.jpg"
+  },
+  {
+    "revision": "fd076f9e9997449994e9982ee9c733e0",
+    "url": "/clickingBad/static/media/waltPatient.fd076f9e.jpg"
+  },
+  {
+    "revision": "ed53b900f8381368b7fb7e76c6f65852",
+    "url": "/clickingBad/static/media/waltPollos.ed53b900.jpg"
+  },
+  {
+    "revision": "51ac43ebdd304d662277638c3df486ed",
+    "url": "/clickingBad/static/media/walterWhite.51ac43eb.jpg"
+  }
+]);
